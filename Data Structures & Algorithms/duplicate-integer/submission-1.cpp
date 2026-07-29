72a5c45a4a7da1663fb7e48acbf00d9a8827e2d5
Ryan Dir <ryandir55@gmail.com>
2026-07-29 16:08:31 -0500

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+       std::unordered_map<int,int> hash;
+       for(int i = 0;i<nums.size();i++){
+            if(hash.find(nums[i]) != hash.end())
+                return true;
+            hash[nums[i]] == i;
+       }
+       return false;
+        
+    }
+};
